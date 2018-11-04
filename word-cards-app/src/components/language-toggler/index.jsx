786b3b6f82f1b6ece295c8c463c8a82bf3fee548
konstantinbel <ksb.bart@gmail.com>
2018-11-04 16:26:46 +0500

    remove locale package flow-bin

--- word-cards-app/src/components/language-toggler/index.jsx
+++ word-cards-app/src/components/language-toggler/index.jsx
@@ -1,10 +1,10 @@
-// // @flow
+// @flow
 
 import React from 'react';
 
-export default props => (
+export default (props: { clickHandler: Function }) => (
   <div>
-    <button onClick={() => { props.clickHandler('ru'); }}>RU</button>
-    <button onClick={() => { props.clickHandler('en'); }}>EN</button>
+    <button type="button" onClick={() => { props.clickHandler('ru'); }}>RU</button>
+    <button type="button" onClick={() => { props.clickHandler('en'); }}>EN</button>
   </div>
 );
